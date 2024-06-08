a9f1868c9ed473f996ef28290dd9290839f1b1fb
training <anshug.ug21.cs@nitp.ac.in>
2024-06-08 12:57:20 +0530

    Adding New Component

--- src/app/my-first-comp/my-first-comp.component.ts
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -9,17 +9,19 @@ export class MyFirstCompComponent {
      name:string =""
      email:string=""
      message:string=""
-     isSubmitted: boolean=true;
+     isSubmitted: boolean=false;
 
      messages:Array<any>=[];
 
      onSubmit(){
+      this.isSubmitted=true;
       this.messages.push({
         "name":this.name,
         "email":this.email,
         "message":this.message
+
       })
-     console.log(this.messages);
+
 
      }
 }

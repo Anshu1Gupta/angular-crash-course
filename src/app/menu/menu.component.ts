82876e745c85a9fa511858362e621baf03e6ee37
training <anshug.ug21.cs@nitp.ac.in>
2024-06-08 17:06:26 +0530

    done with router

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+     constructor(private router: Router){}
+        navigateToAbout():void{
+          this.router.navigate(['about']);
+        }
 
 }
+function navigateToAbout() {
+  throw new Error('Function not implemented.');
+}
+
